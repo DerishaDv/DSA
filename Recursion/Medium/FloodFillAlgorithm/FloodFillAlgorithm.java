a7df50655cfd86aa2a8aa57e2e3d118f20d5d625
DerishaDv <110232393+DerishaDv@users.noreply.github.com>
2024-07-10 17:55:50 +0530

    Update FloodFillAlgorithm.java

--- Recursion/Medium/FloodFillAlgorithm/FloodFillAlgorithm.java
+++ Recursion/Medium/FloodFillAlgorithm/FloodFillAlgorithm.java
@@ -1,6 +1,7 @@
 package Recursion.medium;
 
 public class FloodFillAlgorithm {
+    // using Depth First Search Approach
     public static void main(String[] args) {
         int[][] pixels = {{2, 3, 1, 2, 5, 1},
                 {3, 1, 1, 1, 2, 4},
@@ -22,6 +23,7 @@ public class FloodFillAlgorithm {
     private static void floodFill(int[][] pixels, int fresh, int old, int r, int c, int rows, int cols) {
         if (r < 0 || c < 0 || r >= rows || c >= cols || pixels[r][c] != old) return;
         pixels[r][c] = fresh;
+        //Recursively fill the adjacant elements to the new color
         floodFill(pixels, fresh, old, r - 1, c, rows, cols);
         floodFill(pixels, fresh, old, r + 1, c, rows, cols);
         floodFill(pixels, fresh, old, r, c - 1, rows, cols);
