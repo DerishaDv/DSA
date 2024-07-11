5e1945922eaa1017ae41012ddc3b620458bd0637
DerishaDv <110232393+DerishaDv@users.noreply.github.com>
2024-07-11 16:08:13 +0530

    Create GrayUsingAbstractList.java

---
+++ BitManipulation/GrayCode/GrayUsingAbstractList.java
@@ -0,0 +1,14 @@
+import java.util.AbstractList;
+class GrayUsingAbstractList {
+    public List<Integer> grayCode(int n) {
+        return new AbstractList<Integer> () {
+            public Integer get(int i ){
+                return i^(i>>1);
+            }
+            public int size() {
+                return 1<<n;
+            }
+        };
+        
+    }
+}
